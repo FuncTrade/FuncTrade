fc23c50cd65c207a8f411723676a61811301d053
Zhai Yao <zhai_yao@outlook.com>
2025-08-16 07:27:14 +0800

    scratch idea

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class DataFeed:
+    pass
+
+class Actor:
+    pass
+
+def signal_generate():
+    pass
+
+
+class Pipeline:
+    pass
+
+
+pipeline = DataFeed >> signal_generate >> Actor
+
+
+def play_pipeline(pipeline: List):
+    pass
