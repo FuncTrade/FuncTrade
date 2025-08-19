6abc0f827a0d9470ea4c5bf5984a97e892e16f66
Zhai Yao <zhai_yao@outlook.com>
2025-08-19 10:15:21 +0800

    make seperate trade_logger module

--- function/base.py
+++ function/base.py
@@ -1,12 +1,8 @@
 from abc import abstractmethod
 from dataclasses import dataclass
-import matplotlib.pyplot as plt
 import datetime
-import numpy as np
-import pandas as pd
 
-
-from typing import Any, List, Union, Optional
+from typing import Any, List, Union
 
 
 @dataclass
@@ -51,33 +47,6 @@ class TradeLogger:
     @abstractmethod
     def export(self) -> Any:
         pass
-
-
-class SimplePyplotLogger(TradeLogger):
-    def export(self) -> Any:
-        fig, ax = plt.subplots()
-
-        values = [dt.value for dt in self.data]
-        dts = [dt.timestamp for dt in self.data]
-        dts = np.array(dts, dtype='datetime64[ns]')
-
-        dots = dict(zip(dts, values))
-
-        ax.plot(dts, values, 'bo')
-
-        sig_dts = [sig.timestamp for sig in self.signal]
-        sig_values = [dots[dt] for dt in sig_dts]
-        sig_dts = np.array(sig_dts, dtype='datetime64[ns]')
-
-        for i in range(len(sig_dts)):
-            action = self.signal[i].action
-            x = sig_dts[i]
-            y = sig_values[i]
-            color = 'g' if action == 'buy' else 'r'
-            ax.plot(x, y, color=color)
-            ax.annotate(action, (x, y))
-
-        plt.show()
 
 
 class DataGenerator:
---
+++ function/trade_logger.py
@@ -0,0 +1,34 @@
+from function.base import TradeLogger
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+from typing import Any
+
+
+class SimplePyplotLogger(TradeLogger):
+    def export(self) -> Any:
+        fig, ax = plt.subplots()
+
+        values = [dt.value for dt in self.data]
+        dts = [dt.timestamp for dt in self.data]
+        dts = np.array(dts, dtype='datetime64[ns]')
+
+        dots = dict(zip(dts, values))
+
+        ax.plot(dts, values, 'bo')
+
+        sig_dts = [sig.timestamp for sig in self.signal]
+        sig_values = [dots[dt] for dt in sig_dts]
+        sig_dts = np.array(sig_dts, dtype='datetime64[ns]')
+
+        for i in range(len(sig_dts)):
+            action = self.signal[i].action
+            x = sig_dts[i]
+            y = sig_values[i]
+            color = 'g' if action == 'buy' else 'r'
+            ax.plot(x, y, color=color)
+            ax.annotate(action, (x, y))
+
+        plt.show()
